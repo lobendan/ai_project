d830d557c5820a9e6f69e5eafdf4f52ba5cde781
Daniel Lobenstein <lobenstd@mail.gvsu.edu>
2026-04-21 12:05:17 +0200

    commented out scraper function

--- src/plate_checker_dummy.py
+++ src/plate_checker_dummy.py
@@ -9,11 +9,11 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 # Add the database folder to sys.path
 sys.path.append(project_root)
-from plate_bot.src.database.sqliteDB import SQLiteDB
+# from plate_bot.src.database.sqliteDB import SQLiteDB
 
 class PlateGetter:
     def __init__(self, state, max_age_minutes = 60, min_len=1, max_len=7):
-        self.db = SQLiteDB("plate_bot/src/database/plates.db")
+        # self.db = SQLiteDB("plate_bot/src/database/plates.db")
         self.state = state
         self.max_age = max_age_minutes
         self.min_len = min_len
@@ -36,41 +36,44 @@ class PlateGetter:
 
         return available, not_available
 
-    def check_plate_availability(self, plates):
-        available = []
-        not_available = []
 
-        for plate in plates:
+    # This function used a webscraper, to get actual availability data
+    # Approach is too slow for demo purposes
+    # def check_plate_availability(self, plates):
+    #     available = []
+    #     not_available = []
 
-            if self.db.was_recently_updated(plate, self.state, self.max_age):
-                if self.db.get_word_availability(plate, self.state) == "available":
-                    available.append(plate)
+    #     for plate in plates:
 
-                else:
-                    not_available.append(plate)
+    #         if self.db.was_recently_updated(plate, self.state, self.max_age):
+    #             if self.db.get_word_availability(plate, self.state) == "available":
+    #                 available.append(plate)
 
-            else:
-                self.db.set_last_checked_old(plate, self.state)
-                ticker = 0
-                #waits a max of 20 seconds until word is checked again and still available, if it changes to unavailable it  
-                while(ticker<40):
-                    if self.db.was_recently_updated(plate, self.state):
-                        if self.db.was_recently_updated(plate, self.state, self.max_age):
-                            if self.db.get_word_availability(plate, self.state) == "available":
-                                available.append(plate)
-                                break
+    #             else:
+    #                 not_available.append(plate)
 
-                            else:
-                                not_available.append(plate)
-                                break
-                    else: 
-                        time.sleep(1)
-                        ticker+=1
+    #         else:
+    #             self.db.set_last_checked_old(plate, self.state)
+    #             ticker = 0
+    #             #waits a max of 20 seconds until word is checked again and still available, if it changes to unavailable it  
+    #             while(ticker<40):
+    #                 if self.db.was_recently_updated(plate, self.state):
+    #                     if self.db.was_recently_updated(plate, self.state, self.max_age):
+    #                         if self.db.get_word_availability(plate, self.state) == "available":
+    #                             available.append(plate)
+    #                             break
 
-                if ticker>=40:
-                    print("Warning: Plate availability check timed out for plate:", plate)
-                    not_available.append(plate)    
+    #                         else:
+    #                             not_available.append(plate)
+    #                             break
+    #                 else: 
+    #                     time.sleep(1)
+    #                     ticker+=1
 
-        return available, not_available
+    #             if ticker>=40:
+    #                 print("Warning: Plate availability check timed out for plate:", plate)
+    #                 not_available.append(plate)    
+
+    #     return available, not_available
             
 
